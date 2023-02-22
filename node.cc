c8c4a3c2b055ef1e13d12ad2a9bbc731f7f84e96
Osaki23 <124469633+Osaki23@users.noreply.github.com>
2023-02-22 22:40:47 +0100

    Update node.cc

--- node.cc
+++ node.cc
@@ -1,12 +1,11 @@
 #include "node.hh"
 
-Node::Node(const Jeu & jeu, const courant & j): _nbsim(0), _score(0), _jeu(jeu), _j(j) { }
+Node::Node(const courant & j): _nbsim(0), _score(0), _j(j) { }
 
 Node::Node(std::vector<Node> const & enfant, int nbsim, int score, int nbcoups,int numcoup): _score(score), _nbsim(nbsim), _nbcoups(nbcoups), _enfant(enfant) {
 
 
 //    _player = _game.getCurrentPlayer();
-//     _jeu.playK(numcoup);
 //     _nMoves = int(_game.getMoves().size());
 //      _children.reserve(_nMoves);
 
