f21bfca4cee894a48f0b402694793f9a95f7b53d
Osaki23 <124469633+Osaki23@users.noreply.github.com>
2023-02-22 22:39:11 +0100

    Update node.hh

--- node.hh
+++ node.hh
@@ -18,12 +18,11 @@ public:
     int _score; //somme des récompenses des simulations passés par ce noeud
     int _nbsim; //nombre de simulations passé par ce noeud
     int _nbcoups; //nombre de coups possibles à partir de cet état du jeu
-    Jeu _jeu; //état du jeu de ce noeud;
     std::vector<Node> _enfant; //vecteur de noeuds enfNode(const Jeu & jeu, std::vector<Node> const & enfant, int numcoup)ant
 
     Node(const Node &)= delete; //interdit la copie
     Node(const Jeu & jeu, const courant & j);  //constructeur qui initialise le jeu en début de partie
-    Node(const Jeu & jeu, std::vector<Node> const & enfant, int numcoup); //constructeur pour le déroulement de la partie
+    Node(std::vector<Node> const & enfant, int nbsim, int score, int nbcoups,int numcoup); //constructeur pour le déroulement de la partie
 
 };
 
