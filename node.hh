f25e8607df94c33d363dc3f504345304953b6462
Osaki23 <124469633+Osaki23@users.noreply.github.com>
2023-02-25 22:43:26 +0100

    Update node.hh

--- node.hh
+++ node.hh
@@ -12,17 +12,17 @@ enum class courant {
 class Node
 {
 public:
-    int x;
-    int y;
+    int _x;
+    int _y;
     courant _j; //joueur courant
     int _score; //somme des récompenses des simulations passés par ce noeud
     int _nbsim; //nombre de simulations passé par ce noeud
     int _nbcoups; //nombre de coups possibles à partir de cet état du jeu
     std::vector<Node> _enfant; //vecteur de noeuds enfNode(const Jeu & jeu, std::vector<Node> const & enfant, int numcoup)ant
 
-    Node(const Node &)= delete; //interdit la copie
-    Node(const courant & j);  //constructeur qui initialise le jeu en début de partie
-    Node(std::vector<Node> const & enfant, int nbsim, int score, int nbcoups,int numcoup); //constructeur pour le déroulement de la partie
+    Node();
+    Node(const courant & j, const int & x, const int & y);  //constructeur qui initialise le jeu en début de partie
+    Node(std::vector<Node> const & enfant, int const & nbsim, int const & score, int const & nbcoups, int const & x, int const & y, courant const & j); //constructeur pour le déroulement de la partie
 
 };
 
