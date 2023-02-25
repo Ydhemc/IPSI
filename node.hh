c7857a25183a38c012cd60d716bb8de8d75b9a11
Osaki23 <124469633+Osaki23@users.noreply.github.com>
2023-02-25 23:04:40 +0100

    Update node.hh

--- node.hh
+++ node.hh
@@ -24,6 +24,15 @@ public:
     Node(const courant & j, const int & x, const int & y);  //constructeur qui initialise le jeu en début de partie
     Node(std::vector<Node> const & enfant, int const & nbsim, int const & score, int const & nbcoups, int const & x, int const & y, courant const & j); //constructeur pour le déroulement de la partie
 
+ // methodes   
+ 
+    bool est_feuille() const; // renvoie vrai si le noeud n'a pas d'enfant
+    bool est_terminal() const; //renvoie vrai si le noeud est un état terminal
+    void ajouter_enfant(const Node &enfant); // ajoute un nœud enfant au vecteur _enfant
+    int meilleur_coup() const; //renvoie le meilleur coup à jouer à partir du nœud courant
+    Node & selection(Node const & n); //selectionne le meilleur enfant
+    Node & expansion();
+ 
 };
 
 #endif // ARBRE_HH
