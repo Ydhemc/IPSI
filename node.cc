28fddbbe03e81ab279b0bd3b351fecbaef7ce7d2
Osaki23 <124469633+Osaki23@users.noreply.github.com>
2023-02-25 22:44:08 +0100

    Update node.cc

--- node.cc
+++ node.cc
@@ -1,14 +1,7 @@
 #include "node.hh"
 
-Node::Node(const courant & j): _nbsim(0), _score(0), _j(j) { }
+Node::Node(): _x(0), _y(0), _j(courant::premier), _score(0), _nbsim(0), _nbcoups(0), _enfant({}) {}
 
-Node::Node(std::vector<Node> const & enfant, int nbsim, int score, int nbcoups,int numcoup): _score(score), _nbsim(nbsim), _nbcoups(nbcoups), _enfant(enfant) {
+Node::Node(const courant & j, const int & x, const int & y): _nbsim(0), _score(0), _j(j), _x(x), _y(y), _enfant({}) { }
 
-
-//    _player = _game.getCurrentPlayer();
-//     _nMoves = int(_game.getMoves().size());
-//      _children.reserve(_nMoves);
-
-    //Le nombre de mouvements possibles est récupéré à partir de l'état de jeu actuel et le vecteur "_children" est initialisé avec une capacité pré-allouée égale à "_nMoves".
-}
-
+Node::Node(std::vector<Node> const & enfant, int const & nbsim, int const & score, int const & nbcoups, int const & x, int const & y, courant const & j): _score(score), _nbsim(nbsim), _nbcoups(nbcoups), _x(x), _y(y), _enfant(enfant) { }
