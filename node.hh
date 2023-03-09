9e4fc5fa80759af94ab4f1d807604678871382d3
Abskanunka <126291484+Abskanunka@users.noreply.github.com>
2023-03-09 11:15:00 +0100

    Update node.hh

--- node.hh
+++ node.hh
@@ -51,7 +51,7 @@ public:
    Node meilleur_enfant();
    Node & selection(Node const & n); //selectionne le meilleur enfant
    Node & expansion();
-    Node * fusionnerArbres(Node *,Node *,Node *);
+void * fusionnerArbres(Node const &,Node const &,Node &);
 
 };
 
